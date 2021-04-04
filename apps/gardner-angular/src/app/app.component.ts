bafba3551d457b03172a1f785feb1572738bf3bf
Adam Gardner <adga2012@gmail.com>
2021-04-03 22:52:21 -0600

    initial commit

---
+++ apps/gardner-angular/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gardner-angular-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'gardner-angular';
+}
